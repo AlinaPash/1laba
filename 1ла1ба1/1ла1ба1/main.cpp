0055e52e7272627776068cce81b112e365c678d3
AlinaPash <90476708+AlinaPash@users.noreply.github.com>
2022-01-20 18:11:23 +0300

    Update and rename 1la1ba1/1la1ba1/main.cpp to 1ла1ба1/1ла1ба1/main.cpp

--- 1ла1ба1/1ла1ба1/main.cpp
+++ 1ла1ба1/1ла1ба1/main.cpp
@@ -1,71 +1,54 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 unsigned short pHeight;
-
 unsigned short ReadPersonAge() {
     cout << "Age: ";
     unsigned short age;
     cin >> age;
     return age;
 }
-
 string ReadPersonName() {
     cout << "Name: ";
     string name;
     cin >> name;
     return name;
 }
-
 void ReadPersonHeight() {
     cout << "Height: ";
     cin >> pHeight;
 }
-
 void ReadPersonWeight(unsigned short& weight) {
     cout << "Weight: ";
     cin >> weight;
 }
-
 void ReadPersonSalary(double* salary) {
     cout << "Salary: ";
     cin >> *salary;
 }
-
-void ReadPersonData(string& name, unsigned short& age, string& salary) {
+void ReadPersonData(string& name, unsigned short& age, double& salary) {
     name = ReadPersonName();
     age = ReadPersonAge();
-
-    double pSalary;
-    ReadPersonSalary(&pSalary);
-    salary = to_string(pSalary);
+    ReadPersonSalary(&salary);
 }
-
-
-void ReadPersonData(string& name, unsigned short& age, string& height, string& weight) {
+void ReadPersonData(std::string& name, unsigned short& age, unsigned short& height, unsigned short& weight) {
     name = ReadPersonName();
     age = ReadPersonAge();
 
     ReadPersonHeight();
-    height = to_string(pHeight);
-
-    unsigned short personWeight;
-    ReadPersonWeight(personWeight);
-    weight = to_string(personWeight);
+    height = ::pHeight;
+    ReadPersonWeight(weight);
 }
-
 void WritePersonData(unsigned short age, string& name, const string& height = "", const string& weight = "", const string& salary = "") {
-    cout << "\nName: " << name << "\nAge: " << to_string(age) << "\nHeight: " << height << "\nWeight: " << weight << "\nSalary: " << salary << "\n\n";
+    cout << "\nName: "  << name << "\nAge: " << to_string(age) << "\nHeight: " << height << "\nWeight: " << weight << "\nSalary: " << salary << "\n\n";
 }
-
 int main() {
-    string name, height, weight, salary;
-    unsigned short age;
+    string name;
+    unsigned short height, weight, age;
+    double salary;
     ReadPersonData(name, age, salary);
-    WritePersonData(age, name, "", "", salary);
+    WritePersonData(age, name, "", "", to_string(salary));
     ReadPersonData(name, age, height, weight);
-    WritePersonData(age, name, height, weight);
+    WritePersonData(age, name, to_string(height), to_string(weight));
     return 0;
 }
